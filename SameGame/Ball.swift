0b07b9465e98c88dfe2d1c8b16de283b787fd275
Joel Burton <joel@joelburton.com>
2020-09-09 12:09:03 -0700

    feat: working initial version

---
+++ SameGame/Ball.swift
@@ -0,0 +1,35 @@
+//
+// Created by Joel Burton on 9/8/20.
+// Copyright (c) 2020 Joel Burton. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Ball: SKSpriteNode {
+    var x: Int!
+    var y: Int!
+    var clusterId = 0
+
+    func configure(color: String, x: Int, y: Int) {
+        self.name = color
+        self.physicsBody?.angularDamping = 0
+        self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.width / 2)
+        setPosition(x: x, y: y)
+    }
+
+    func setPosition(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+        self.position = CGPoint(x: x * 46 + 50, y: y * 46 + 50)
+    }
+
+    func spin() {
+        self.physicsBody?.angularVelocity = 5
+        self.physicsBody?.angularDamping = 0
+    }
+
+    func stopSpin() {
+        self.physicsBody?.angularDamping = 3
+    }
+}
--- SameGame/GameScene.swift
+++ SameGame/GameScene.swift
@@ -1,89 +1,237 @@
-//
-//  GameScene.swift
-//  SameGame
-//
-//  Created by Joel Burton on 9/8/20.
-//  Copyright © 2020 Joel Burton. All rights reserved.
-//
-
 import SpriteKit
 import GameplayKit
 
+/** SameGame scene: manages UI and game logic. */
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    let ballColors = ["Purple", "Cyan", "Green", "Yellow", "Red"]
+
+    let numRows = 14
+    let numCols = 21
+    var numBalls = 0
+
+    // game grid contains ball or nil for empty slot
+    var grid = [[Ball?]]()
+
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet { scoreLabel.text = "Score: \(score)" }
+    }
+    var gameOverMsg: SKSpriteNode!
+    var gameOver = false
+
+    // "cluster" is group of connected-by-color balls; the current cluster is the one
+    // that is selected now (is rotating for users). All clusters are identified after
+    // any change in game grid, so that (a) selecting a new cluster happens very quickly
+    // in the UI and (b) we know when the game ends (there are no more greater-than-one
+    // clusters). A currentCluster of 0 means nothing is selected.
+    var currentClusterId = 0
+    var clusters: [[Ball]] = [[]]
+
+
+    /** Set up this scene. */
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.physicsWorld.gravity = .zero
+
+        let background = SKSpriteNode(imageNamed: "background")
+        background.zPosition = -1
+        background.blendMode = .replace
+        background.position = CGPoint(x: 512, y: 384)
+        self.addChild(background)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+
+        startGame()
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+
+    /** Start (or restart) game. */
+    func startGame() {
+        grid.forEach { row in row.forEach { ball in ball?.removeFromParent() } }
+        grid = [[Ball?]]()
+
+        for y in 0..<numRows {
+            var row = [Ball]()
+            for x in 0..<numCols {
+                let color = ballColors.randomElement()!
+                let ball = Ball(imageNamed: "ball\(color)")
+                ball.configure(color: color, x: x, y: y)
+                self.addChild(ball)
+                row.append(ball)
+            }
+            grid.append(row)
         }
+
+        numBalls = numRows * numCols
+        findClusters()
+
+        currentClusterId = 0
+        score = 0
+        gameOver = false
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
+    /** Handle touch: selects cluster if no currently selected; otherwise, removes cluster. */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        // debug state of clusters/board by touching the score label
+        if nodes(at: location).first == scoreLabel {
+            dump()
+            return
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
+        if gameOver {
+            gameOverMsg.removeFromParent()
+            startGame()
+        }
+
+        guard let ball = nodes(at: location).first as? Ball else { return }
+        if !clusters[currentClusterId].contains(ball) {
+            selectCluster(x: ball.x, y: ball.y);
+        } else {
+            removeCluster()
+        }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+
+    /** Select cluster and spin them (deselects current cluster. */
+    func selectCluster(x: Int, y: Int) {
+        clusters[currentClusterId].forEach { $0.stopSpin() }
+        currentClusterId = grid[y][x]!.clusterId
+        // only groups of >1 can be removed, so don't rotate clusters w/only 1 ball
+        if clusters[currentClusterId].count > 1 { clusters[currentClusterId].forEach { $0.spin() } }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
+    /** Find clusters (neighbors-of-same-color) on board and update clusters property.
+     *
+     * Called on game start, and after any board move.
+     */
+    func findClusters() {
+        grid.forEach { row in row.forEach { ball in ball?.clusterId = 0 } }
+        clusters = [[]]
+
+        // "0 cluster" is "no cluster selected", so all the real clusters will get an id starting
+        // with 1. Neighboring balls with the same color share a cluster, so we graph-traverse
+        // to identify clusters.
+        var newClusterId = 1
+
+        for y in 0..<numRows {
+            for x in 0..<numCols {
+                guard let ball = grid[y][x] else { continue }
+                // if a ball already has a cluster, it's already fulled accounted for
+                guard ball.clusterId == 0 else { continue }
+
+                ball.clusterId = newClusterId
+                clusters.append([])
+                newClusterId += 1
+
+                // DFS: recursively explore neighbors, starting here
+                var toVisit = [ball]
+                while toVisit.count > 0 {
+                    let visiting = toVisit.removeLast()
+
+                    if visiting.name == ball.name {
+                        visiting.clusterId = ball.clusterId
+                        clusters[ball.clusterId].append(visiting)
+
+                        if visiting.y > 0 {
+                            let neighbor = grid[visiting.y - 1][visiting.x]
+                            if neighbor?.clusterId == 0 { toVisit.append(neighbor!) }
+                        }
+                        if visiting.y < numRows - 1 {
+                            let neighbor = grid[visiting.y + 1][visiting.x]
+                            if neighbor?.clusterId == 0 { toVisit.append(neighbor!) }
+                        }
+                        if visiting.x > 0 {
+                            let neighbor = grid[visiting.y][visiting.x - 1]
+                            if neighbor?.clusterId == 0 { toVisit.append(neighbor!) }
+                        }
+                        if visiting.x < numCols - 1 {
+                            let neighbor = grid[visiting.y][visiting.x + 1]
+                            if neighbor?.clusterId == 0 { toVisit.append(neighbor!) }
+                        }
+                    }
+                }
+            }
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
+    /** Remove currently-selected cluster from board. */
+    func removeCluster() {
+        let currentCluster = clusters[currentClusterId]
+        for ball in currentCluster {
+            ball.removeFromParent()
+            grid[ball.y][ball.x] = nil
+        }
+
+        numBalls -= currentCluster.count
+        score += currentCluster.count * currentCluster.count
+        if numBalls < 50 {
+            score += (50 - numBalls) * 100
+        }
+
+        currentClusterId = 0
+        shiftRemainingBalls()
+        findClusters()
+
+        if clusters.allSatisfy({ cluster in cluster.count < 2 }) {
+            gameOver = true
+            gameOverMsg = SKSpriteNode(imageNamed: "gameOver")
+            gameOverMsg.position = CGPoint(x: 512, y: 384)
+            gameOverMsg.zPosition = 2
+            self.addChild(gameOverMsg)
+        }
     }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
+    /** Shift balls down/over when a cluster disappears. */
+    func shiftRemainingBalls() {
+        // work from left-to-right (x goes down), bottom to top (y goes up)
+        for x in 0..<numCols {
+            var emptyStreak = 0
+            for y in 0..<numRows {
+                if let ball = grid[y][x] {
+                    if emptyStreak > 0 {
+                        grid[y][x] = nil
+                        grid[y - emptyStreak][x] = ball
+                        ball.setPosition(x: x, y: y - emptyStreak)
+                    }
+                } else {
+                    emptyStreak += 1
+                }
+            }
+        }
+        // when all balls in column are gone, detect and shift everything left
+        for var x in 0..<numCols {
+            let col = (0..<numRows).map { y in grid[y][x] }
+            if col.allSatisfy({ $0 == nil }) {
+                if x < numCols - 1 {
+                    for y in 0..<numRows {
+                        grid[y][x] = grid[y][x + 1]
+                        if let ball = grid[y][x] { ball.setPosition(x: x, y: y) }
+                        grid[y][x + 1] = nil
+                    }
+                    x -= 1
+                }
+            }
+        }
+    }
+
+    // debugging cluster creation & grid state:
+    func dump() {
+        print("\nclusterId count [ball:(color, x, y), ...]")
+        for (id, cluster) in clusters.enumerated() {
+            print(id, cluster.count, cluster.map { ($0.name!, $0.x!, $0.y! )})
+        }
+        print("\ngridX=ballX gridY=ballY name clusterId")
+        for y in 0..<numRows {
+            for x in 0..<numCols {
+                if let b = grid[y][x] {
+                    print("\(x)=\(b.x!), \(y)=\(b.y!) \(b.name!) \(b.clusterId)")
+                } else {
+                    print(x, y, "-")
+                }
+            }
+        }
     }
 }
