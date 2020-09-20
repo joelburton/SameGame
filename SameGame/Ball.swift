af6af073ae962835aa40d80fa28ec0903a09ff8c
Joel Burton <joel@joelburton.com>
2020-09-19 19:34:16 -0700

    docs: improve docs on Ball class

--- SameGame/Ball.swift
+++ SameGame/Ball.swift
@@ -6,14 +6,16 @@ final class Ball: SKSpriteNode {
 
     var x: Int!
     var y: Int!
+
+    /** During game, this is updated with all balls in the same color cluster. */
     var cluster: LinkedList<Ball>? = nil
+    // (list is same ref for all balls in cluster, so using a reference type for this)
 
     /** Make a randomly-colored ball at x, y. */
     convenience init(x: Int, y: Int) {
         let color = Self.colors.randomElement()!
         self.init(imageNamed: "ball\(color)")
         self.name = color
-        self.physicsBody?.angularDamping = 0
         self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.width / 2)
         setPosition(x: x, y: y)
     }
